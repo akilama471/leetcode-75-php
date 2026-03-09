20e036a63796411882ad78483ec817e8a3b95c59
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-03-09 18:30:00 +0530

    daily-temperatures-solution

---
+++ daily-temperatures/solution.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+*/
+
+class Solution {
+
+    /**
+     * @param Integer[] $temperatures
+     * @return Integer[]
+     */
+    function dailyTemperatures($temperatures) {
+        $n = count($temperatures);
+        $res = array_fill(0, $n, 0);
+        $stack = [];
+
+        for ($i = 0; $i < $n; $i++) {
+            while (!empty($stack) && $temperatures[end($stack)] < $temperatures[$i]) {
+                $idx = array_pop($stack);
+                $res[$idx] = $i - $idx;
+            }
+            $stack[] = $i;
+        }
+
+        return $res;        
+    }
+}
